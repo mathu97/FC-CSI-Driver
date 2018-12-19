0c5f911720c15a6fa455834bfb65597afd977689
Mathusan Selvarajah <mathusans52@gmail.com>
2018-12-19 13:23:59 -0500

    Updating driver to use kubernetes-csi/csi-lib-fc

--- pkg/fcDriver/fc.go
+++ pkg/fcDriver/fc.go
@@ -1,10 +1,25 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package fc
 
 import (
 	"encoding/json"
 	"fmt"
 	"github.com/container-storage-interface/spec/lib/go/csi/v0"
-	"github.com/mathu97/csi-connectors/fibrechannel"
+	"github.com/kubernetes-csi/csi-lib-fc/fibrechannel"
 	"k8s.io/kubernetes/pkg/util/mount"
 	"k8s.io/kubernetes/pkg/volume/util"
 )
@@ -49,11 +64,11 @@ func getFCInfo(req *csi.NodePublishVolumeRequest) (*fcDevice, error) {
 
 }
 
-func getFCDiskMounter(req *csi.NodePublishVolumeRequest) *fibrechannel.FCMounter {
+func getFCDiskMounter(req *csi.NodePublishVolumeRequest) FCMounter {
 	readOnly := req.GetReadonly()
 	fsType := req.GetVolumeCapability().GetMount().GetFsType()
 	mountOptions := req.GetVolumeCapability().GetMount().GetMountFlags()
-	return &fibrechannel.FCMounter{
+	return FCMounter{
 		ReadOnly:     readOnly,
 		FsType:       fsType,
 		MountOptions: mountOptions,
--- pkg/fcDriver/nodeserver.go
+++ pkg/fcDriver/nodeserver.go
@@ -1,8 +1,23 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package fc
 
 import (
 	"github.com/container-storage-interface/spec/lib/go/csi/v0"
-	"github.com/mathu97/csi-connectors/fibrechannel"
+	"github.com/kubernetes-csi/csi-lib-fc/fibrechannel"
 	"golang.org/x/net/context"
 )
 
@@ -27,13 +42,13 @@ func (ns *fcNodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePubl
 		return nil, err
 	}
 	fcDisk = *fcDevice
-	disk, connectError := fibrechannel.Connect(*fcDevice.connector)
+	disk, connectError := fibrechannel.Attach(*fcDevice.connector, &fibrechannel.OSioHandler{})
 
 	fcDisk.disk = disk
 
 	//Mount
 	fcmounter := getFCDiskMounter(req)
-	fibrechannel.MountDisk(*fcmounter, disk)
+	MountDisk(fcmounter, disk)
 
 	if connectError != nil {
 		return nil, connectError
@@ -44,7 +59,7 @@ func (ns *fcNodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePubl
 
 func (ns *fcNodeServer) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
 
-	if err := fibrechannel.Disconnect(*fcDisk.connector, fcDisk.disk); err != nil {
+	if err := fibrechannel.Detach(fcDisk.disk, &fibrechannel.OSioHandler{}); err != nil {
 		return nil, err
 	}
 
